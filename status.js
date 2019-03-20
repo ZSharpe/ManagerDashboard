215e889f6117f1b9bac297f5c6fe4283f539d2ac
Zach <zach.sharpe5@gmail.com>
2019-03-20 07:51:15 -0500

    Add JSON feed for status event history and upcoming events via statuspage.io. Format using innerHTML method

--- status.js
+++ status.js
@@ -1,15 +1,24 @@
 var statusWindow = document.querySelectorAll("#statusHistory")[0];
 var statusMaintenance = document.querySelectorAll("#statusMaintenance")[0];
 
-fetch('https://4lzycl8t1x0c.statuspage.io/api/v2/scheduled-maintenances.json')
+fetch('https://4lzycl8t1x0c.statuspage.io/api/v2/scheduled-maintenances/upcoming.json')
     .then(function(response){ 
         return response.json();
     }).then(function(myJson){
         JSON.stringify(myJson);
-        var maintJSON = document.querySelectorAll("#statusMaintenance")[0]
-        myJson.scheduled_maintenances.forEach(element => {
-            
-            console.log(element.scheduled_for);
-            maintJSON.textContent = element.scheduled_for;
-        });
+        var maintJSON = document.querySelectorAll("#statusMaintenance")[0];
+        maintJSON.innerHTML = "<br>" + myJson.scheduled_maintenances[0].name;
+    })
+
+    fetch('https://4lzycl8t1x0c.statuspage.io/api/v2/incidents.json')
+    .then(function(response){ 
+        return response.json();
+    }).then(function(myJson){
+        JSON.stringify(myJson);
+        
+        var maintJSON = document.querySelectorAll("#statusHistory")[0];
+        for(var element=0; element<10; element++ ){
+            console.log(myJson);
+            maintJSON.innerHTML += myJson.incidents[element].created_at.substr(0,10) + " <br><a href=" + myJson.incidents[element].shortlink +">" + myJson.incidents[element].name + "</a><br><br>";
+        };
     })
