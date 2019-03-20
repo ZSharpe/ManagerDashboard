0784d27e44aecb4b12fff69a279f7d381182bf69
Zach <zach.sharpe5@gmail.com>
2019-03-20 07:13:24 -0500

    Alter padding on navbar CSS, add promise statement to fetch maintenence data.

--- metrics.js
+++ metrics.js
@@ -20,12 +20,11 @@ function mondayMetricsStart(){
 
 // Open tabs for the second half of Monday reporting (slide updates).
 function mondayMetricsStartSecond() {
-
     var windowInstructions = window.open("https://myemma.atlassian.net/wiki/spaces/CustomerSupport/pages/665518081/Updating+the+weekly+supporter+digest+Weekly+CX+Update+numbers");
     var windowStaffingCalc = window.open("https://docs.google.com/spreadsheets/d/16SQoyuG6OZFXiys5yISt0IvNYRD3-eL8pE9Xp20Udkc/edit#gid=0");
     var windowSlide = window.open("https://docs.google.com/presentation/d/1N7urCk9qoW-8o2NdnUqAQMV-BBhOOMl-CCLg2td-U5g/edit#slide=id.g4e9506b5ca_0_0");
     var windowDeputy = window.open("https://2390fa18082540.na.deputy.com/#roster");
-    var windowCasesClosed = window.open("https://campaignmonitor.my.salesforce.com/console");
+    var windowCasesClosed = window.open("https://campaignmonitor.my.salesforce.com/00O1J000005qXny?cancelURL=%2F00O1J000005qXny");
     windowInstructions.focus;
 }
 
--- status.js
+++ status.js
@@ -1,21 +1,15 @@
-// var maintenanceHistory = $.getJSON("https://4lzycl8t1x0c.statuspage.io/api/v2/scheduled-maintenances.json",
-//     function () {
-//         console.log("Maintenance history retrieved successfully!")
-//     }
-// );
-
-var maintData = fetch('https://4lzycl8t1x0c.statuspage.io/api/v2/scheduled-maintenances.json')
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(myJson){
-        JSON.stringify(myJson);
-        return myJson;
-    });
-
-// JSON.parse(maintData);
-
 var statusWindow = document.querySelectorAll("#statusHistory")[0];
 var statusMaintenance = document.querySelectorAll("#statusMaintenance")[0];
 
-// statusWindow.textContent = maintenancesData;
+fetch('https://4lzycl8t1x0c.statuspage.io/api/v2/scheduled-maintenances.json')
+    .then(function(response){ 
+        return response.json();
+    }).then(function(myJson){
+        JSON.stringify(myJson);
+        var maintJSON = document.querySelectorAll("#statusMaintenance")[0]
+        myJson.scheduled_maintenances.forEach(element => {
+            
+            console.log(element.scheduled_for);
+            maintJSON.textContent = element.scheduled_for;
+        });
+    })
