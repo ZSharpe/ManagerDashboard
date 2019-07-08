40e30240cfadb4e7eee841dda73ce4f5208d8d83
Zach <zach.sharpe5@gmail.com>
2019-07-08 12:14:32 -0500

    Update metrics links to open correct resources and pare down unecessary links.

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var statusWindow = document.querySelectorAll("#statusHistory")[0];
 var statusMaintenance = document.querySelectorAll("#statusMaintenance")[0];
+var schedule = document.querySelectorAll("#scheduleBlock");
+$.get("/api/v1/my/roster" , function(rosters){ console.log("My Rosters" , rosters);})
+
 
 fetch('https://4lzycl8t1x0c.statuspage.io/api/v2/scheduled-maintenances/upcoming.json')
     .then(function(response){ 
@@ -7,10 +10,10 @@ fetch('https://4lzycl8t1x0c.statuspage.io/api/v2/scheduled-maintenances/upcoming
     }).then(function(myJson){
         JSON.stringify(myJson);
         var maintJSON = document.querySelectorAll("#statusMaintenance")[0];
-        maintJSON.innerHTML = "<br>" + myJson.scheduled_maintenances[0].name;
+        maintJSON.innerHTML = "<br>";
     })
 
-    fetch('https://4lzycl8t1x0c.statuspage.io/api/v2/incidents.json')
+fetch('https://4lzycl8t1x0c.statuspage.io/api/v2/incidents.json')
     .then(function(response){ 
         return response.json();
     }).then(function(myJson){
@@ -22,3 +25,5 @@ fetch('https://4lzycl8t1x0c.statuspage.io/api/v2/scheduled-maintenances/upcoming
             maintJSON.innerHTML += myJson.incidents[element].created_at.substr(0,10) + " <br><a href=" + myJson.incidents[element].shortlink +">" + myJson.incidents[element].name + "</a><br><br>";
         };
     })
+
+    // schedule.textcontent($.get("2390fa18082540.na.deputy.com/api/v1/my/roster" , function(rosters){ console.log("My Rosters" , rosters);}));    
--- metrics.js
+++ metrics.js
@@ -10,7 +10,6 @@ var metricsClickThursday = thursdayMetricsButton.addEventListener("click", thurs
 
 // Open tabs for the first half of Monday reporting (supporter weekly metrics).
 function mondayMetricsStart(){
-    var windowInstructions = window.open("https://myemma.atlassian.net/wiki/spaces/CustomerSupport/pages/665518081/Updating+the+weekly+supporter+digest+Weekly+CX+Update+numbers");
     var windowDeputy = window.open("https://2390fa18082540.na.deputy.com/#report");
     var windowSalesforce = window.open("https://na74.salesforce.com/00O1J000005qXny?cancelURL=%2F00O1J000005qXny");
     var windowTalkdesk = window.open("https://campaignmonitor.mytalkdesk.com/#monitoring/schedules/5b9bad188fd0250009ff4431/executions");
@@ -20,16 +19,10 @@ function mondayMetricsStart(){
 
 // Open tabs for the second half of Monday reporting (slide updates).
 function mondayMetricsStartSecond() {
-    var windowInstructions = window.open("https://myemma.atlassian.net/wiki/spaces/CustomerSupport/pages/665518081/Updating+the+weekly+supporter+digest+Weekly+CX+Update+numbers");
     var windowStaffingCalc = window.open("https://docs.google.com/spreadsheets/d/16SQoyuG6OZFXiys5yISt0IvNYRD3-eL8pE9Xp20Udkc/edit#gid=0");
-    var windowSlide = window.open("https://docs.google.com/presentation/d/1N7urCk9qoW-8o2NdnUqAQMV-BBhOOMl-CCLg2td-U5g/edit#slide=id.g4e9506b5ca_0_0");
+    var surveyResponses = window.open("https://www.getfeedback.com/surveys/677515/summary");
+    var managerSlides = window.open("https://docs.google.com/presentation/d/1uvvDSunbkUNSS2eU0pjZRqBvJRVBjf3UG-YoMcpZ-pA/edit");
     var windowDeputy = window.open("https://2390fa18082540.na.deputy.com/#roster");
     var windowCasesClosed = window.open("https://campaignmonitor.my.salesforce.com/00O1J000005qXny?cancelURL=%2F00O1J000005qXny");
     windowInstructions.focus;
-}
-
-// Open tabs for Thursday slide updates.
-function thursdayMetricsStart(){
-    var windowInstructions = window.open("https://myemma.atlassian.net/wiki/spaces/CustomerSupport/pages/665518081/Updating+the+weekly+supporter+digest+Weekly+CX+Update+numbers");
-    var windowSlide = window.open("https://docs.google.com/presentation/d/1N7urCk9qoW-8o2NdnUqAQMV-BBhOOMl-CCLg2td-U5g/edit#slide=id.g3d1b80a0d1_2_0");
 }
